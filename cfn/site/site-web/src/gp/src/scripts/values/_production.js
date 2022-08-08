1e2d0366f94dbbad6377b9e15985d4165165566d
hattor-wni <hattor@wni.com>
2022-08-08 13:39:41 +0900

    CII API URLの削除

--- cfn/site/site-web/src/gp/src/scripts/values/_production.js
+++ cfn/site/site-web/src/gp/src/scripts/values/_production.js
@@ -4,6 +4,5 @@ export default {
   TSUNAMI_WEBSOCKET_URL: 'wss://g3k6uu8vn0.execute-api.ap-northeast-1.amazonaws.com/prod',
   SECURE_DATA_URL: '/customer',
   EXPO_RANDOM_POINT_URL: 'https://wni-data-expo-api.wni.com/random',
-  API_URL: '/api',
-  CII_CALC_URL: 'https://b01-cii.seapln-osr.prod-aws.wni.com/v1/cii-calc'
+  API_URL: '/api'
 }
